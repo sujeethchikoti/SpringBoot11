8018dd3f58af9e3aea659f18f99f1bb6a0bffed5
sujeeth <sujeethc@infinite.com>
2023-11-20 11:05:21 +0530

    commit1

--- src/main/java/org/cap/demo/Day2SpringBootRestApplication.java
+++ src/main/java/org/cap/demo/Day2SpringBootRestApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Day2SpringBootRestApplication {
-
+ 
 	public static void main(String[] args) {
 		SpringApplication.run(Day2SpringBootRestApplication.class, args);
 	}
